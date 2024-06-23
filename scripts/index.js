5b702b32ce657d3b856bf292d716a6485253d3fc
Gavin Reid <gavingq94@gmail.com>
2024-06-23 15:56:47 -0400

    refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -28,14 +28,49 @@ const initialCards = [cardData];
 
 console.log(initialCards);
 
+/* -------------------------------------------------------------------------- */
+/*                                  elements                                  */
+/* -------------------------------------------------------------------------- */
 const profileEditButton = document.querySelector("#profile__edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const modalButtonClose = document.querySelector("#modal-button-close");
+const profileTitle = document.querySelector("#profile-title");
+const profileDescription = document.querySelector("#profile-description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
 
+/* -------------------------------------------------------------------------- */
+/*                                  functions                                 */
+/* -------------------------------------------------------------------------- */
+
+function classPopup() {
+  profileEditModal.classList.remove("modal__opened");
+}
+
+/* -------------------------------------------------------------------------- */
+/*                               event handlers                               */
+/* -------------------------------------------------------------------------- */
+function handleProlileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  classPopup();
+}
+
+/* -------------------------------------------------------------------------- */
+/*                               event listeners                              */
+/* -------------------------------------------------------------------------- */
 profileEditButton.addEventListener("click", () => {
   profileEditModal.classList.add("modal__opened");
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
 });
 
 modalButtonClose.addEventListener("click", () => {
   profileEditModal.classList.remove("modal__opened");
 });
+
+profileEditForm.addEventListener("submit", handleProlileEditSubmit);
