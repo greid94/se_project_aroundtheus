e1a7b0e7418368bcd526e38ec6d67035af8ec710
Gavin Reid <gavingq94@gmail.com>
2024-06-23 17:22:38 -0400

    project 4 update

--- scripts/index.js
+++ scripts/index.js
@@ -41,13 +41,31 @@ const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
-
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 /* -------------------------------------------------------------------------- */
 /*                                  functions                                 */
 /* -------------------------------------------------------------------------- */
 
 function classPopup() {
   profileEditModal.classList.remove("modal__opened");
+}
+
+function getCardElement(cardData) {
+  //clone the template element with all its content and store it in a cardElement variable
+  const cardElement = cardTemplate.cloneNode(true);
+  //access the card title and image and store them in variables
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__text");
+  //set the path to the image to the link field of the object
+  cardImageEl.src = cardData.link;
+  //set the image alt text to the name field of the object
+  cardImageEl.alt = cardData.name;
+  //set the card title to the name field of the object, too
+  cardTitleEl.textContent = cardData.name;
+  //return the ready HTML element with the filled-in data
+  return cardElement;
 }
 
 /* -------------------------------------------------------------------------- */
@@ -74,3 +92,8 @@ modalButtonClose.addEventListener("click", () => {
 });
 
 profileEditForm.addEventListener("submit", handleProlileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.prepend(cardElement);
+});
