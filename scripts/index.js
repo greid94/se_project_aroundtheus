091476b46c2500a233d1e5d052937c2292743d58
Gavin Reid <gavingq94@gmail.com>
2024-07-18 00:01:09 -0400

    update for project 5

--- scripts/index.js
+++ scripts/index.js
@@ -29,25 +29,35 @@ const initialCards = [
 /*                                  elements                                  */
 /* -------------------------------------------------------------------------- */
 const profileEditButton = document.querySelector("#profile__edit-button");
-const profileEditModal = document.querySelector("#profile-edit-modal");
-const modalButtonClose = document.querySelector("#modal-button-close");
+const profileEditModal = document.querySelector("#edit-modal");
+const addCardModal = document.querySelector("#add-card-modal");
+const profileModalClose = profileEditModal.querySelector("#modal-button-close");
+const addCardModalClose = addCardModal.querySelector("#modal-button-close");
 const profileTitle = document.querySelector("#profile-title");
 const profileDescription = document.querySelector("#profile-description");
 const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
-const profileEditForm = profileEditModal.querySelector(".modal__form");
+const profileEditForm = document.querySelector(".modal__form");
 const cardListEl = document.querySelector(".cards__list");
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
+const addNewCardBtn = document.querySelector(".profile__add-button");
+
 /* -------------------------------------------------------------------------- */
 /*                                  functions                                 */
 /* -------------------------------------------------------------------------- */
 
-function closePopup() {
-  profileEditModal.classList.remove("modal_opened");
+function closePopup(modal) {
+  modal.classList.remove("modal_opened");
 }
+function openModal(modal) {
+  modal.classList.add("modal_opened");
+}
+//profileEditModal.classList.add("modal_opened");
+//profileTitleInput.value = profileTitle.textContent;
+//profileDescriptionInput.value = profileDescription.textContent;
 
 function getCardElement(initialCards) {
   //clone the template element with all its content and store it in a cardElement variable
@@ -78,14 +88,22 @@ function handleProlileEditSubmit(e) {
 /* -------------------------------------------------------------------------- */
 /*                               event listeners                              */
 /* -------------------------------------------------------------------------- */
-profileEditButton.addEventListener("click", () => {
-  profileEditModal.classList.add("modal_opened");
-  profileTitleInput.value = profileTitle.textContent;
-  profileDescriptionInput.value = profileDescription.textContent;
+
+addNewCardBtn.addEventListener("click", () => {
+  openModal(addCardModal);
 });
 
-modalButtonClose.addEventListener("click", () => {
-  closePopup();
+profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+  openModal(profileEditModal);
+});
+
+profileModalClose.addEventListener("click", () => {
+  closePopup(profileEditModal);
+});
+addCardModalClose.addEventListener("click", () => {
+  closePopup(addCardModal);
 });
 
 profileEditForm.addEventListener("submit", handleProlileEditSubmit);
